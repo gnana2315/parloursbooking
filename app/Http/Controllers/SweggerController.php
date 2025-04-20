2f6903754a160697182a6d12e8bb2da667622d78
Gnanatheeban <gnana2315@gmail.com>
2025-04-20 20:48:59 +0530

    swagger controller update

--- app/Http/Controllers/SweggerController.php
+++ app/Http/Controllers/SweggerController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use L5Swagger\GeneratorFactory;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class SweggerController extends Controller
 {
     public function generate()
     {
         try {
-            // Run the Swagger generator programmatically
-            $generator = app()->make(GeneratorFactory::class);
-            $generator->generateDocs();
+            Artisan::call('l5-swagger:generate');
 
             return response()->json([
                 'status' => 'success',
