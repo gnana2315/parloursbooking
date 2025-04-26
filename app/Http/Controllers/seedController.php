3712a8a1c76cefba613afec34b80bde9a31a8e9a
Gnanatheeban <gnana2315@gmail.com>
2025-04-26 22:20:17 +0530

    conflict on api doc

--- app/Http/Controllers/seedController.php
+++ app/Http/Controllers/seedController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class seedController extends Controller
 {
     public function seedFromController()
     {
-        // Call the db:seed command
-        Artisan::call('db:seed');
+        try {            
+            // Call the db:seed command
+            Artisan::call('db:seed');
 
-        return response()->json([
-            'message' => 'Database seeded successfully from controller!'
-        ]);
+            return response()->json([
+                'message' => 'Database seeded successfully from controller!'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()], 500);
+        }
     }
 }
