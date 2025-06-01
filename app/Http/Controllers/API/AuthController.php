ff32ea73bc72c984177f4a1c7b185e2c63c3d969
Gnanatheeban <gnana2315@gmail.com>
2025-06-01 22:23:41 +0530

    fix: remove extra space in userForgotPassword method declaration

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -529,8 +529,7 @@ class AuthController extends Controller
      * )
      */
 
-    public function userForgotPassword (Request $request){
-
+    public function userForgotPassword(Request $request){
         $request->validate(
             [
                 'phone_no' => 'required|exists:users,pbu_mobileno',
