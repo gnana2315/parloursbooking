3a0ad8cee04033c2189f41728fea223976d12947
Gnanatheeban <gnana2315@gmail.com>
2025-06-05 14:48:34 +0530

    fix: update userLogin method to output user object for debugging

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -457,8 +457,8 @@ class AuthController extends Controller
 
         // Find user by mobile number
         $user = User::where('pbu_mobileno', $request->phone_no)->first();
-        var_dump($user->getAuthPassword());
-        var_dump(Hash::make($request->password));die();
+        dd($user);
+        // var_dump(Hash::make($request->password));die();
         //Check if user verified the mobile no
         if($user->pbu_mobileno_verified_at == null){
             return response()->json(['message' => 'User not verfied yet.'], 500);
