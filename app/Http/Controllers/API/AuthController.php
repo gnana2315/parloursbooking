6d43b6e94bfccc25915caca7e32c4586e6b2568f
Gnanatheeban <gnana2315@gmail.com>
2025-06-05 14:45:17 +0530

    fix: update userLogin method to retrieve the first user by mobile number and use var_dump for debugging

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -456,8 +456,8 @@ class AuthController extends Controller
         );
 
         // Find user by mobile number
-        $user = User::where('pbu_mobileno', $request->phone_no);
-        dd($user);
+        $user = User::where('pbu_mobileno', $request->phone_no)->first();
+        var_dump($user);
         var_dump(Hash::make($request->password));die();
         //Check if user verified the mobile no
         if($user->pbu_mobileno_verified_at == null){
