1139d458d9e7c94db47713ecf0edc6e816ce45b2
Gnanatheeban <gnana2315@gmail.com>
2025-06-05 14:58:51 +0530

    fix: add debugging statement to check customer retrieval in addCustomerFavourite method

--- app/Http/Controllers/API/CustomersController.php
+++ app/Http/Controllers/API/CustomersController.php
@@ -133,7 +133,7 @@ class CustomersController extends Controller
     public function addCustomerFavourite(Request $request){
         $user = auth()->user();
         $customer = customer::where('pbc_user_id', $user->pbu_id)->first();
-
+        dd($customer);
         if(!$customer){
             return response()->json([
                 'message' => 'Customer not found',
