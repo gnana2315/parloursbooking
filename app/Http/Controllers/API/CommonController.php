13fcb527eef54f562bf4181b6996c24108154acc
Gnanatheeban <gnana2315@gmail.com>
2025-06-08 15:53:32 +0530

    test

--- app/Http/Controllers/API/CommonController.php
+++ app/Http/Controllers/API/CommonController.php
@@ -661,8 +661,7 @@ class CommonController extends Controller
     public function getAllPromoCodes(){
 
         $promoCodes = promoCode::where('pbpc_status', 1)->get();
-        print_r('<pre>');
-        print_r($promoCodes);
+        var_dump($promoCodes);
         die();
         $promos = [];
         
