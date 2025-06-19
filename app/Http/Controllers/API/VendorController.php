1020333b274eb10f65600962250ddba75b04b3de
Gnanatheeban <gnana2315@gmail.com>
2025-06-19 21:20:44 +0530

    test

--- app/Http/Controllers/API/VendorController.php
+++ app/Http/Controllers/API/VendorController.php
@@ -843,8 +843,8 @@ class VendorController extends Controller
         $user = auth()->user();
         $vendor_results = vendors::where('pbv_id', $vendor_id)
                 ->join('vendor_config', 'vendor_config.pbvc_vendorid', '=', 'vendor.pbv_id')
-                // ->join('services', 'services.pbs_vendor_id', '=', 'vendor.pbv_id') 
-                //->where('vendor.pbv_status', 1)
+                ->leftJoin('services', 'services.pbs_vendor_id', '=', 'vendor.pbv_id') 
+                ->where('vendor.pbv_status', 1)
                 ->get();        
         
         if (!$vendor_results) {
