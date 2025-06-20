c66b3dd0286f6485d105dd28dd0c9fad3ea9c8ad
Gnanatheeban <gnana2315@gmail.com>
2025-06-20 14:37:34 +0530

    test

--- app/Http/Controllers/API/VendorController.php
+++ app/Http/Controllers/API/VendorController.php
@@ -844,13 +844,13 @@ class VendorController extends Controller
         $vendor_results = vendors::join('vendor_config', 'vendor_config.pbvc_vendorid', '=', 'vendor.pbv_id')
                 ->join('vendor_standard_availability', 'vendor_standard_availability.pbvsa_vendor_id', '=', 'vendor.pbv_id')
                 ->join('cities', 'cities.pbc_cid', '=', 'vendor.pbv_city')
-                ->join('services', 'services.pbs_vendor_id', '=', 'vendor.pbv_id')
+                // ->join('services', 'services.pbs_vendor_id', '=', 'vendor.pbv_id')
                 ->select(
                     'vendor.*',
                     'vendor_config.*',
                     'vendor_standard_availability.*',
                     'cities.*',
-                    'services.*'
+                    // 'services.*'
                 )
                 ->where('pbv_id', $vendor_id)
                 ->where('vendor.pbv_status', 1)
