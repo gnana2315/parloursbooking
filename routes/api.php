b9c76308242303c334a7e3705e4f173bd557f944
Gnanatheeban <gnana2315@gmail.com>
2025-08-26 23:14:16 +0530

    test

--- routes/api.php
+++ routes/api.php
@@ -84,7 +84,7 @@ Route::middleware(['auth:sanctum', 'validate.token'])->group(function () {
     Route::get('/vendor/{vendor_id}', [VendorController::class, 'getVendorById' ]);
     Route::get('/getVendorDocuments',[VendorController::class,'getVendorDocuments']);
     Route::get('/getVendorAvailability', [VendorController::class, 'getVendorAvailability']);
-    Route::get('/vendor/bankDetails', [VendorController::class, 'getVendorBankDetails']);
+    Route::get('/getVendorBankDetails', [VendorController::class, 'getVendorBankDetails']);
     Route::get('/vendor/details', [VendorController::class, 'getVendorDetails']);
 
     //common
