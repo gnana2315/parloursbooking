ed9da4677006b657bb103a77c9abfcc0f0d56289
Gnanatheeban <gnana2315@gmail.com>
2025-10-04 21:15:53 +0530

    test

--- app/Http/Controllers/API/VendorController.php
+++ app/Http/Controllers/API/VendorController.php
@@ -1443,8 +1443,7 @@ class VendorController extends Controller
         $vendor_results = vendors::with(['city', 'availability', 'vendorDocuments'])
             ->where('pbv_id', $user->pbu_vid)
             ->where('pbv_status', 1)
-            ->first();
-        dd($user->pbu_vid);    
+            ->get();   
         dd($vendor_results);
         if (!$vendor_results) {
             return response()->json(['message' => 'Vendor not found'], 404);
