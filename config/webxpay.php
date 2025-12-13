119a1e0eb3172d09f6be9bde8afd691027304536
Gnanatheeban <gnana2315@gmail.com>
2025-12-13 20:55:00 +0530

    Add debug statement in addOnlineBooking_v1 method for testing purposes

--- config/webxpay.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'api_url'         => env('WEBXPAY_API_URL', ''),
-    'username'        => env('WEBXPAY_USERNAME', ''),
-    'password'        => env('WEBXPAY_PASSWORD', ''),
-    'merchant_number' => env('WEBXPAY_MERCHANT_NUMBER', ''),
-];
