aff944336014244e29b624923abe06e25fe4470e
Gnanatheeban <gnana2315@gmail.com>
2025-12-20 21:29:33 +0530

    Implement booking creation and payment processing with WebXPay integration method 2 - Fix 3

--- app/Services/WebXPayService.php
+++ app/Services/WebXPayService.php
@@ -52,7 +52,7 @@ class WebXPayService
     public function getUserDetails($jwt)
     {
         $response = Http::withToken($jwt)->get($this->baseUrl . "GetUserDetails");
-        dd($response);
+        dd($response->json());
         // Check if response is ok and contains data
         if ($response->successful() && isset($response->json()['data'])) {
             return $response->json()['data'];
