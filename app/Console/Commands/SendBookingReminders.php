b4e719d0892fb941680cf4af57ed164c2bc2b1d3
theebans <gnana2315@gmail.com>
2026-01-03 22:34:44 +0530

    add reminder cron - fix 3

--- app/Console/Commands/SendBookingReminders.php
+++ app/Console/Commands/SendBookingReminders.php
@@ -26,7 +26,7 @@ class SendBookingReminders extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(OneSignalService $oneSignalService)
     {
         $now = Carbon::now();
 
