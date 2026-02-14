22d2f6c76e0192c85037c8299c57c56ace7f2b63
theebans <gnana2315@gmail.com>
2026-02-14 14:43:45 +0530

    test

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -666,7 +666,7 @@ class BookingController extends Controller
                 ], 409);
             }
 
-            $pbb_ref_no = 'PBA-' . date('ymdHis');
+            $pbb_ref_no = uniqid(date('ymdHis'));
 
             // 5️⃣ Create booking
             $addbooking = Booking::create([
@@ -1029,7 +1029,7 @@ class BookingController extends Controller
             ], 409);
         }
 
-        $pbb_ref_no = '_PBW-' . date('ymdHis');
+        $pbb_ref_no = uniqid(date('ymdHis'));
 
         $addbooking = Booking::create([
             'pbb_vendor_id' => $vendor->pbv_id,
